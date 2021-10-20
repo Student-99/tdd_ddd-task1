53005da371721ac8353f4657893aac962864dc9f
Student <ekbstudent@yandex-team.ru>
2021-10-21 01:07:00 +0500

    тесты с реализацией

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -1,24 +1,34 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class CreditCalculator {
 
 
     private static Double monthlyRate(Double interestRate) {
-
+        return interestRate / 12 / 100;
     }
 
     private static Double totalBid(Double interestRate, int term) {
-
+        Double result = Math.pow(1 + monthlyRate(interestRate), term);
+        return new BigDecimal(result).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
 
-    public static Double monthlyPayment(Integer sum, int term, Double interestRate) {
 
+    public static Double monthlyPayment(Integer sum, int term, Double interestRate) {
+        Double totalBid = totalBid(interestRate, term);
+        Double result = sum * monthlyRate(interestRate) * totalBid / (totalBid - 1);
+        return new BigDecimal(result).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
 
     public static Double calculationOfTotalAmountToBeRefundedToBank(Integer sum, int term,
                                                                     Double interestRate) {
+        Double result = monthlyPayment(sum, term, interestRate) * term;
+        return new BigDecimal(result).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
 
     }
 
     public static Double calculationOfOverpaymentForEntirePeriod(Integer sum, int term, Double interestRat) {
-
+        Double result = monthlyPayment(sum, term, interestRat) * term - sum;
+        return new BigDecimal(result).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        int sumCredit = 5_000_000;
+        int term = 36;
+        double interestRat = 5.99;
+        System.out.println(String.format("Ежемесячный платеж: %.2f", CreditCalculator.monthlyPayment(sumCredit, term,
+                interestRat)));
+        System.out.println(String.format("Расчет переплаты за весь период: %.2f",
+                CreditCalculator.calculationOfOverpaymentForEntirePeriod(sumCredit, term, interestRat)));
+        System.out.println(String.format("Общая сумма возврата: %.2f",
+                CreditCalculator.calculationOfTotalAmountToBeRefundedToBank(sumCredit, term, interestRat)));
     }
 }
